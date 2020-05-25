5a65524de76cda64b565a6f494199312a5de8806
Jessica <jehcriss42@gmail.com>
2020-05-25 20:07:30 -0300

    getting started with angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: `
+    <div><h1>{{pageTitle}}</h1>
+        <div> My first component</div>
+    </div>    
+  `
+})
+export class AppComponent {
+  pageTitle: string = 'Acme product Management'
+}
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Angular: Getting Started';
+// }
