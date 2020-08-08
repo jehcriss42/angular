fc82e3342c90000a7df6a5cf286378fa7109f252
jehcriss42 <jehcriss42@gmail.com>
2020-08-07 22:11:31 -0300

    added http response

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit {
     imageWidth = 50;
     imageMargin = 2;
     showImage = false;
+    errorMessage; 
 
     _listFilter: string;
     get listFilter(): string {
@@ -40,8 +41,13 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-      this.products = this.productService.getProducts();
-      this.filteredProducts = this.products;
+      this.productService.getProducts().subscribe({
+        next: products => { 
+          this.products = products;
+          this.filteredProducts = this.products;
+        }, 
+        error: err => this.errorMessage = err
+      });
     }
 
 } 
